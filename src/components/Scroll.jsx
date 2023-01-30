31682dfe5d602fae13761d6273ce484554ee758c
brunoFernandes21 <47353649+brunoFernandes21@users.noreply.github.com>
2023-01-30 13:18:32 +0000

    Fixed indentation on scrollbar jsx

--- src/components/Scroll.jsx
+++ src/components/Scroll.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
 
 const Scroll = ({ children }) => {
-    const styles = {
-        overflowY: 'scroll',
-        border: '5px solid black',
-        height: '550px',
-        borderRadius: '10px'
-    }
+  const styles = {
+    overflowY: 'scroll',
+    border: '5px solid black',
+    height: '550px',
+    borderRadius: '10px'
+  }
   return (
     <div style={styles} className='scrollbar'>
-        { children }
+      { children }
     </div>
   )
 }
