0d0388f0f4581fb2f7320dd83bc486a947eb313c
brunoFernandes21 <47353649+brunoFernandes21@users.noreply.github.com>
2023-02-04 13:41:07 +0000

    Fixed title and input width on xs devices

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -4,7 +4,7 @@ const SearchBox = ({ searchFieldValue, handleChange }) => {
   return (
     <div className="pa2 mb3">
       <input
-        className="pa3 br3 ba b--blue bg-lightest-blue"
+        className="w-60 pa3 br3 ba b--blue bg-lightest-blue"
         type="text"
         name="search"
         value={searchFieldValue}
